c50749ebd5454e46b24426b6a8342368f52f1dd0
Loïc d'Eyssautier <loic.d-eyssautier@epitech.eu>
2022-05-30 16:07:15 +0200

    [FIX] TestingLibraryElementError on github actions

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+
+
+afterEach(cleanup);
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText("/learn react/i")
-  expect(linkElement).toBeInTheDocument();
+  function tree() {
+    return render(
+      <Router>
+        <App/>
+      </Router>
+    )
+  }
+
+  it('should match with snapshot', () => {
+    expect(tree()).toMatchSnapshot();
+  });
+
 });
