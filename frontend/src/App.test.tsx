5a2cdaa0eaaa7fa2c4cc23094b370d0f9653881d
Loïc d'Eyssautier <loic.d-eyssautier@epitech.eu>
2022-05-30 15:51:19 +0200

    [FIX] TestingLibraryElementError on github actions

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -4,8 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  function linkElement() {
-    return screen.getByText(/learn react/i);
-  }
-  expect(linkElement()).toBeInTheDocument();
+  const linkElement = screen.getByText('/learn react/i');
+  expect(linkElement).toBeInTheDocument();
 });
